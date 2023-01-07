4ad488d8821096ea2d86effc3d44291032a3b4f8
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-01-07 20:30:31 +0200

    SQL

---
+++ Sequlize/Practice/models/order.js
@@ -0,0 +1,14 @@
+const {Sequelize, INTEGER} = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Order = sequelize.define('order', {
+    id: {
+        type: INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Order;
---
+++ Sequlize/Practice/models/product.js
@@ -0,0 +1,28 @@
+const {Sequelize, INTEGER, STRING, DOUBLE} = require('sequelize');
+
+const sequelize = require('../util/database');
+
+
+const Product = sequelize.define("product", {
+    id: {
+        type: INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: STRING,
+    price: {
+        type: DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: STRING,
+        allowNull: false
+    },
+    description: {
+        type: STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Product;
