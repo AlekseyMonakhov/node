ac803cd77c9601e72c88c3db729aa377b85b4a5f
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-01-09 01:29:55 +0200

    upd

--- noSQL-MongoDB/Practice/models/product.js
+++ noSQL-MongoDB/Practice/models/product.js
@@ -1,53 +1,79 @@
-const mongodb = require('mongodb');
-const {getDb} = require('../util/database');
-
+const { ObjectId } = require("mongodb");
+const { getDb } = require("../util/database");
 
 class Product {
-    constructor(title, price, description, imageUrl) {
+    constructor(title, price, description, imageUrl, id, userId) {
         this.title = title;
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
+        this._id = id ? new ObjectId(id) : null;
+        this.userId = userId;
     }
 
     static fetchAll() {
         const db = getDb();
-        return db.collection('products')
+        return db
+            .collection("products")
             .find()
             .toArray()
             .then((products) => {
-                console.log(products)
                 return products;
             })
             .catch((err) => {
-                console.log(err)
-            })
+                console.log(err);
+            });
     }
 
     static findById(prodId) {
         const db = getDb();
-        return db.collection('products')
-            .find({_id: new mongodb.ObjectId(prodId)})
+        return db
+            .collection("products")
+            .find({ _id: new ObjectId(prodId) })
             .next()
             .then((product) => {
-                console.log(product)
-                return product
+                return product;
             })
             .catch((err) => {
-                console.log(err)
+                console.log(err);
+            });
+    }
+
+    static deleteById(prodId) {
+        const db = getDb();
+        return db
+            .collection("products")
+            .deleteOne({
+                _id: ObjectId(prodId),
             })
+            .then((result) => {
+                console.log("Deleted");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     save() {
         const db = getDb();
-        return db.collection('products')
-            .insertOne(this)
+        let dbOp;
+        if (this._id) {
+            dbOp = db.collection("products").updateOne(
+                { _id: this._id },
+                {
+                    $set: this,
+                }
+            );
+        } else {
+            dbOp = db.collection("products").insertOne(this);
+        }
+        return dbOp
             .then((result) => {
                 console.log(result);
             })
             .catch((err) => {
                 console.log(err);
-            })
+            });
     }
 }
 
