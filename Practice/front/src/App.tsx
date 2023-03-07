253d3c41934b3ee4ded5de7bc2ecae4af6dd7a20
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-03-08 00:15:27 +0200

    upd

--- Practice/front/src/App.tsx
+++ Practice/front/src/App.tsx
@@ -1,86 +1,66 @@
-import styles from './styles/main.module.scss'
-import { FC, FormEvent, useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useRef, useState } from 'react'
+import classes from './styles/main.module.scss'
+import { motion, useAnimationControls } from 'framer-motion'
 
-interface Client {
-    name: string,
-    age: number,
-    id: string
-}
-
-interface ClientProps extends Client{
-    resetClient(s:string): void,
-}
-
-
-const Client: FC<ClientProps> = ({ name, age, id,resetClient }) => {
-    return (
-        <div className={styles.client}>
-            <h1>{name}</h1>
-            <h2>{age}</h2>
-            <button onClick={() =>resetClient(id)}>Delete</button>
-        </div>
-    )
-}
-
-function App() {
-    const [clients, setClients] = useState<Client[]>([])
-    const [client, setClient] = useState<Client>({ name: '', age: 22, id:"" })
-    console.log(clients)
-    const addClient = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        axios.post('http://localhost:3001/login', client).then(({ data }) => {
-            setClients((prev)=> [...prev, data]);
-        })
-    }
-
-    const resetAll = () => {
-        axios.get('http://localhost:3001/reset').then(() => setClients([]))
-    }
-    const resetClient = (id:string) => {
-        axios.get('http://localhost:3001/reset/' + id).then((data) => {
-            setClients((prev) => prev.filter((clients) => clients.id !== id))
-        })
-    }
+const App = () => {
+    const [coordinates, setCoordinates] = useState<{ x: number; y: number }>({
+        x: 0,
+        y: 0,
+    })
+    const control = useAnimationControls()
+    const container = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        axios
-            .get<Client[]>('http://localhost:3001')
-            .then(({ data: clients }) => {
-                setClients(clients)
+        control
+            .start({
+                x: coordinates.x,
+                y: coordinates.y,
             })
-            .catch((err) => {
-                console.log(err)
+            .then(() => {
+                console.log(coordinates)
             })
-    }, [])
+    }, [coordinates.x, coordinates.y])
 
     return (
-        <div className={styles.main}>
-            {clients.length ? (
-                clients.map((client, index) => (
-                    <Client resetClient={resetClient} key={index} name={client.name} age={client.age} id={client.id}/>
-                ))
-            ) : (
-                <h2>no Clients yet</h2>
-            )}
-            <form onSubmit={addClient}>
-                <input
-                    onChange={(e) =>
-                        setClient((prev) => ({ ...prev, name: e.target.value}))
+        <div className={classes.main} ref={container}>
+            <div className={classes.container}>
+                <button
+                    onClick={() =>
+                        setCoordinates((prev) => ({ ...prev, x: prev.x - 150 }))
                     }
-                    type={'text'}
-                />
-                <input
-                    onChange={(e) =>
-                        setClient((prev) => ({ ...prev, age: +e.target.value}))
+                >
+                    left
+                </button>
+                <button
+                    onClick={() =>
+                        setCoordinates((prev) => ({ ...prev, x: prev.x + 150 }))
                     }
-                    type={'number'}
-                />
-                <button>add clients</button>
-            </form>
-            <button onClick={resetAll}>reset all client</button>
+                >
+                    right
+                </button>
+                <button
+                    onClick={() =>
+                        setCoordinates((prev) => ({ ...prev, y: prev.y - 150 }))
+                    }
+                >
+                    up
+                </button>
+                <button
+                    onClick={() =>
+                        setCoordinates((prev) => ({ ...prev, y: prev.y + 150 }))
+                    }
+                >
+                    down
+                </button>
+            </div>
+            <motion.div
+                drag
+                animate={control}
+                dragConstraints={container}
+                transition={{ type: 'tween', duration: 1 }}
+                whileDrag={{ scale: 2 }}
+            ></motion.div>
         </div>
     )
 }
-
 export default App
